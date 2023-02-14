72563c7a525debd9dc458b10c8ed17af837dc765
Matheus Boechat <matheusfgb@gmail.com>
2023-02-13 22:50:12 -0300

    requisito 5

--- script.js
+++ script.js
@@ -2,14 +2,31 @@ const letterBtn = document.getElementById('criar-carta');
 const input = document.getElementById('carta-texto');
 const p = document.querySelector('p');
 
-letterBtn.addEventListener('click', () => {
-  while (p.firstChild) {
-    p.removeChild(p.firstChild);
+const verifyInput = () => {
+  if (input.value === '') {
+    p.innerHTML = 'Por favor, digite o conteúdo da carta.';
+    return false;
   }
-  const inputTextArray = input.value.split(' ');
+  const inputTextArray = input.value.split('');
   for (let i = 0; i < inputTextArray.length; i += 1) {
-    const span = document.createElement('span');
-    span.innerText = inputTextArray[i];
-    p.appendChild(span);
+    if (inputTextArray[i] !== ' ') {
+      return true;
+    }
+  }
+  p.innerHTML = 'Por favor, digite o conteúdo da carta.';
+  return false;
+};
+
+letterBtn.addEventListener('click', () => {
+  if (verifyInput()) {
+    while (p.firstChild) {
+      p.removeChild(p.firstChild);
+    }
+    const inputTextArray = input.value.split(' ');
+    for (let i = 0; i < inputTextArray.length; i += 1) {
+      const span = document.createElement('span');
+      span.innerText = inputTextArray[i];
+      p.appendChild(span);
+    }
   }
 });
